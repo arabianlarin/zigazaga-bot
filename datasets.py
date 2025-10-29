c6964f9f3aa6c9b9438d7be908b20d77d220e4d4
Rakhat <66788294+arabianlarin@users.noreply.github.com>
2025-10-29 16:12:42 +0500

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -75,5 +75,6 @@ def get_player_data():
     full_data['Tackles Won %'] = round(full_data['TklW_Tackles']*100/full_data['Tkl_Tackles'], 2)
     full_data['Shots on Target %'] = round(full_data['SoT_per_90_Standard']/full_data['Sh_per_90_Standard'] * 100, 2)
     full_data['diff'] = round(full_data['G_minus_PK'] - full_data['npxG_Expected'], 2)
+    full_data['web_name'] = players['web_name'].apply(lambda x: fbr.normalize_name(x))
 
     return full_data.fillna(0)
