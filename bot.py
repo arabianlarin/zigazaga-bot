ff3d6af4d4cd114e385aeb24842f2f4744603a88
Rakhat <66788294+arabianlarin@users.noreply.github.com>
2025-10-29 14:46:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def compare(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("compare", compare))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+#app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
 if __name__ == "__main__":
     app.run_webhook(
