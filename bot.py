a7d03ad329f86b05564a939099b2b964e628ef96
Rakhat <66788294+arabianlarin@users.noreply.github.com>
2025-10-29 15:37:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,6 @@ import os
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 print("Render TELEGRAM_TOKEN:", TELEGRAM_TOKEN)
 
-pio.kaleido.scope.chromium_args += ("--no-sandbox", "--disable-dev-shm-usage")
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hi!')
 
